4093c565bf818c1a92072f6b54635aee0c2cd300
Karina Adamiak <kara.ada1985@gmail.com>
2023-01-05 09:10:17 +0100

    wait

--- cypress/e2e/TS1.cy.js
+++ cypress/e2e/TS1.cy.js
@@ -2,7 +2,12 @@ describe ('my first test',() => {
     it('visit main page and click top menu bottom', function(){
         cy.visit('https://fabrykatestow.pl/')
         cy.get('#nv-primary-navigation-main').contains('Newsletter').click()
+       
         cy.url().should('eq','https://fabrykatestow.pl/ciekawostki/')
+        
+        cy.wait(2000)
+        
+        cy.go('back')
 
     })
     
