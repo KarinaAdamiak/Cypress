d2a6a274ebd0f463eab980817c7b7d5535a5acc5
Karina Adamiak <kara.ada1985@gmail.com>
2023-01-05 08:14:22 +0100

    changes in test

--- cypress/e2e/TS1.cy.js
+++ cypress/e2e/TS1.cy.js
@@ -1,6 +1,8 @@
 describe ('my first test',() => {
     it('visit main page and click top menu bottom', function(){
         cy.visit('https://fabrykatestow.pl/')
+        cy.get('#nv-primary-navigation-main').contains('Newsletter').click
+
 
     })
     
